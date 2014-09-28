9b6e074afb6d6f2113fa258f1693761068e7e813
diedev <dam_135@yahoo.com>
2014-09-27 18:44:29 -0600

    ni pi
    asd

---
+++ pajama_dev/caso02.js
@@ -0,0 +1,2 @@
+load('rt/util.js');function factorialCola(a){return (function (x){return (function (n, c){if((function (x){return (patListTest)([function (x){return (x[0]===0);}, function (x){return true;}], x);})(n))return r[1];
+else return (c)(n);})(x, function (x){return (function (n, c){if((function (x){return (patListTest)([function (x){return true;}, function (x){return true;}], x);})(n))return 
