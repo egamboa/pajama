2b7449a5f36aa13309b00c49257d28e8e9008ed9
Esteban Gamboa <estebangam1@gmail.com>
2014-10-08 23:49:37 -0600

    no .js in git hub

--- pajama_dev/caso02.js
+++
@@ -1,3 +0,0 @@
-load('rt/util.js');function factorialCola(a){return (function (x){return (function (n, c){if((function (x){return (patListTest)([function (x){return (x[0]===0);}, function (x){return true;}], x);})(n))return x[1];
-else return (c)(n);})(x, function (x){return (function (n, c){if((function (x){return (patListTest)([function (x){return true;}, function (x){return true;}], x);})(n))return FUNCALL_TO_BE_DONE();
-else return (c)(n);})(x, fail);});})(a);}n(1, 
