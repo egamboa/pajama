48d25ddda299f917533af7887473d9159a77bc20
B3rnal <bernalmatus@gmail.com>
2014-10-17 21:37:08 -0600

    mas js

---
+++ pajama_dev/case_result/caso01_2.js
@@ -0,0 +1,16 @@
+load('rt/util.js');
+
+function factorialCola(a) {
+    return (function(x) {
+        return (function(n, c) {
+            if ((function(x) {
+                    return (patListTest)([function(x) {
+                        return (x[0] === 0);
+                    }, any], x);
+                })(n)) return (function(x) {
+                return foo(x[1], 1);
+            })(n);
+            else return (c)(n);
+        })(x, fail);
+    })(a);
+}
