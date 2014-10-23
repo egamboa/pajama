6dfe30699375f35515d1723546e28a3f1e5bd15f
Esteban Gamboa <estebang@thehangar.cr>
2014-10-22 22:28:32 -0600

    beautify JS

--- pajama_dev/case_result/caso00.js
+++ pajama_dev/case_result/caso00.js
@@ -1,2 +1,14 @@
-load('rt/util.js');function caso0(n){return (function (x){return (function (n, c){if((function (x){return (x===0);})(n))return (function (x){return false;})(n);
-else return (c)(n);})(x, fail);})(n);}
+load('rt/util.js');
+
+function caso0(n) {
+    return (function(x) {
+        return (function(n, c) {
+            if ((function(x) {
+                return (x === 0);
+            })(n)) return (function(x) {
+                return false;
+            })(n);
+            else return (c)(n);
+        })(x, fail);
+    })(n);
+}
--- pajama_dev/case_result/caso01.js
+++ pajama_dev/case_result/caso01.js
@@ -1,3 +1,23 @@
-load('rt/util.js');function caso1(n){return (function (x){return (function (n, c){if((function (x){return (x===0);})(n))return (function (x){return false;})(n);
-else return (c)(n);})(x, function (x){return (function (n, c){if((function (x){return (x===1);})(n))return (function (x){return true;})(n);
-else return (c)(n);})(x, fail);});})(n);}
+load('rt/util.js');
+
+function caso1(n) {
+    return (function(x) {
+        return (function(n, c) {
+            if ((function(x) {
+                return (x === 0);
+            })(n)) return (function(x) {
+                return false;
+            })(n);
+            else return (c)(n);
+        })(x, function(x) {
+            return (function(n, c) {
+                if ((function(x) {
+                    return (x === 1);
+                })(n)) return (function(x) {
+                    return true;
+                })(n);
+                else return (c)(n);
+            })(x, fail);
+        });
+    })(n);
+}
