b0acdc95b16f95a965cd943324b7e6aec7eaf676
diedev <dam_135@yahoo.com>
2014-10-23 13:00:25 -0600

    cambios de casos!

--- pajama_dev/rt/util.js
+++ pajama_dev/rt/util.js
@@ -2,67 +2,57 @@ function MyException(msg, fail){
 	this.msg = msg;
 	this.fail = fail;
 }
-
-
 MyException.prototype.toString = function()this.msg;
 
 
 function patListTest(lp, n){
-
-
-
-	
 	return Array.isArray(n)
-			&& n.length == lp.length
-			&& lp.reduce(function(z, p) z && p(n), true);
-};
+	&& n.length == lp.length
+	&& lp.reduce(function(z, p) z && p(n), true);
 
+};
 
 function patListTestEmpty(x)
 	Array.isArray(x) && x.length == 0;
 
 function patObjTest(lp, o){
-
-	if(typeof o !='object') return false;
+	if(typeof o != 'object') return false;
 	var keys = Object.keys(lp);
 	return keys.reduce(function(z, k)z&&lp[k](o), true);
 }
 function patObjectTestEmpty(o){
 	return typeof o =='object' && Object.keys(o).length==0;
 }
-var FAIL = new java.lang.Exception("Pattern matching Exception");
+var FAIL = new MyException("Pattern Matching Exception", true);
 function fail(){
 	throw FAIL;
 }
 
 function any(x)true;
 
-
 function expectEQ(expr, r){
-	try{
-		return (eval(expr).toString() === r) 
-	} catch(e){
-		return false;
-	}
-}
+        try{
+                var res = (eval(expr).toString() === r.toString());
+                if (res) return true;
+                throw "FAILED";
+        } catch(e){
+                throw "expecteEQ failed:"+ expr + "!="+ r + " "+ e.toString() ;
+        }
+};
 
-
-function expectFAIL(expr){	
-	try{
-		eval(expr);
-		throw new MyException("exception to FAIL but it did not: " + expr, true);
-	}
-	catch(e){
-		if (e.fail) return true;
-		throw e.toString();
-	}
-}
-
+var FAIL_MSG = "expected to FAIL but it did not: ";
+function expectFAIL(expr){      
+        try{
+                eval(expr);
+                throw new MyException(expr);
+        } catch(e){
+            if (e.fail) return true;
+                throw FAIL_MSG + e.toString();
+        }
+};
 
 function FUNCALL_TO_BE_DONE(){
 	return "+something needed to be done!!!";
 }
-
-function any(x)true;
 
 function unshift(a, x){a.unshift(x);return a;}
